44b22e24834bcba9f3d332b82c13141c3a24ecfc
Artr Ctrs <arthur.catrisse@hotmail.com>
2020-12-03 22:47:35 +0100

    update views

--- backend/pigeon_app/views/views.py
+++ backend/pigeon_app/views/views.py
@@ -1,5 +1,21 @@
 from django.http import JsonResponse
-from ..models.test import Test
+from ..models import Player
+from ..models import Pigeon
+import logging
+
 
 def index(request):
+    logging.debug("------"+str(request))
     return JsonResponse({ "status": "I'm here" })
+
+def create_test_player(request):
+    logging.debug("------"+str(request))
+    player = Player(username='test')
+    logging.debug("------"+str(player))
+    player.save()
+    return JsonResponse({ "status": player.username })
+
+def get_test_players(request):
+    logging.debug("------"+str(request))
+    players = Player.objects.all()
+    return JsonResponse({ "status": players })
