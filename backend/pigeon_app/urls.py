4c26476d03db2c14b2fdce644d595bfab230b691
Cécilia Huang <cecilia.huang.pro@gmail.com>
2020-12-06 17:08:43 +0100

    fix api links

--- backend/pigeon_app/urls.py
+++ backend/pigeon_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import views
 
 urlpatterns = [
+    path(r'api/test/', views.index, name='test'),
     path(r'api/create_test_player/', views.create_test_player, name='create_test_player'),
     path(r'api/get_test_players/', views.get_test_players, name='get_test_players'),
 ]
