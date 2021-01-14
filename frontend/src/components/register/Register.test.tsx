4b17371ec1cb4faca95e0dd4ea0922ba86a9939c
floralyellow <cecilia.huang.pro@gmail.com>
2021-01-14 20:18:40 +0100

    delete react

--- frontend/src/components/register/Register.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Register from './Register';
-
-test('renders learn react link', () => {
-  render(<Register />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
