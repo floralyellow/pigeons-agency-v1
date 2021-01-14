0b607985d68909fc15b2856d25c0d13c00ff6664
floralyellow <cecilia.huang.pro@gmail.com>
2021-01-14 20:18:24 +0100

    delete react

--- frontend/src/components/dashboard/Dashboard.test.tsx
+++ frontend/src/components/dashboard/Dashboard.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Dashboard from './Dashboard';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Dashboard />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
