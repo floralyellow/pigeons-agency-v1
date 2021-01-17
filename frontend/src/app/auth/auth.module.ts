7eb21cb20a7a074c64d5c708b5764b7ce8a6b64e
floralyellow <cecilia.huang.pro@gmail.com>
2021-01-17 14:58:53 +0100

    init auth configuration

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RegisterComponent } from './register/register.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
 
 
 @NgModule({
   declarations: [
+    RegisterComponent,
+    AuthentificationComponent
   ],
   imports: [
     BrowserModule,
   ],
-  providers: [],
-  bootstrap: []
+  providers: [
+  ],
+  bootstrap: [
+  ],
+  exports: [
+    RegisterComponent,
+    AuthentificationComponent
+  ],
 })
 export class AuthModule { }
---
+++ frontend/src/app/auth/authentification/authentification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authentification',
+  templateUrl: './authentification.component.html',
+  styleUrls: ['./authentification.component.scss']
+})
+export class AuthentificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
