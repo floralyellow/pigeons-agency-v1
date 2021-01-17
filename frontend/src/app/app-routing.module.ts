40697e2b2ea96096c080481fdd550cdd383dd1a1
floralyellow <cecilia.huang.pro@gmail.com>
2021-01-17 14:58:53 +0100

    init auth configuration

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent, AuthentificationComponent } from './auth';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "register",
+    component : RegisterComponent
+  },
+  {
+    path : "authentification",
+    component : AuthentificationComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
