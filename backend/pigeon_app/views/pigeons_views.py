d9e09dc838fd1a0df7bacaba1f60265904ea80da
Artr Ctrs <arthur.catrisse@hotmail.com>
2021-11-24 15:24:11 +0100

    fix exp_type restriction

--- backend/pigeon_app/views/pigeons_views.py
+++ backend/pigeon_app/views/pigeons_views.py
@@ -44,7 +44,7 @@ class PigeonView(APIView):
         expedition_type = request.POST.get('exp_type')
         if not expedition_lvl.isdigit() or not int(expedition_lvl) in range(1,30):
             return JsonResponse({'message': 'Error: invalid input lvl'})
-        if not expedition_type.isdigit() or not int(expedition_lvl) in range(1,4):
+        if not expedition_type.isdigit() or not int(expedition_type) in range(1,4):
             return JsonResponse({'message': 'Error: invalid input type'})
 
         expeditions = pigeon_service.create_pigeon(request.user, expedition_lvl, expedition_type)
