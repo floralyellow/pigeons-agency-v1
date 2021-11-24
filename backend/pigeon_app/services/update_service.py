5d477b1b9f3ff3ee34e43f1f791c743a05f181a9
Artr Ctrs <arthur.catrisse@hotmail.com>
2021-11-24 12:02:24 +0100

    fixes set_in_team & max_feathers

--- backend/pigeon_app/services/update_service.py
+++ backend/pigeon_app/services/update_service.py
@@ -1,6 +1,7 @@
 from django.db.models import Sum
 from ..models import Pigeon
 from datetime import datetime,timezone
+from django.db import transaction
 from ..models import TR_Lvl_info
 from ..models import Player
 import math
@@ -10,25 +11,26 @@ def update_user_values(user):
         """
         update player droppings and seeds
         """
-        drop_min = Pigeon.objects.filter(player__user = user.id, is_open = True, is_sold=False).aggregate(tot_drop=Sum('droppings_minute')).get('tot_drop') or 0
+        with transaction.atomic():
+            drop_min = Pigeon.objects.filter(player__user = user.id, is_open = True, is_sold=False).aggregate(tot_drop=Sum('droppings_minute')).get('tot_drop') or 0
 
-        delta_time = datetime.now(timezone.utc) - user.player.last_updated_at
-        delta_minutes = delta_time.total_seconds() / 60
+            delta_time = datetime.now(timezone.utc) - user.player.last_updated_at
+            delta_minutes = delta_time.total_seconds() / 60
 
-        droppings_in_delta_time = math.ceil(delta_minutes * drop_min)
-        lvl_info = TR_Lvl_info.objects.get(lvl=user.player.lvl)
-        seeds_in_delta_time = math.ceil(delta_minutes * lvl_info.seeds_minute)
-        logging.debug('mins'+str(delta_minutes))
+            droppings_in_delta_time = math.ceil(delta_minutes * drop_min)
+            lvl_info = TR_Lvl_info.objects.get(lvl=user.player.lvl)
+            seeds_in_delta_time = math.ceil(delta_minutes * lvl_info.seeds_minute)
+            logging.debug('mins'+str(delta_minutes))
 
-        # droppings
-        if user.player.droppings < lvl_info.max_droppings:
-            droppings_to_apply = user.player.droppings +  droppings_in_delta_time
-            user.player.droppings = min(droppings_to_apply, lvl_info.max_droppings)
+            # droppings
+            if user.player.droppings < lvl_info.max_droppings:
+                droppings_to_apply = user.player.droppings +  droppings_in_delta_time
+                user.player.droppings = min(droppings_to_apply, lvl_info.max_droppings)
 
-        #seeds
-        if user.player.seeds < lvl_info.max_seeds:
-            seeds_to_apply = user.player.seeds + seeds_in_delta_time
-            user.player.seeds = min(seeds_to_apply,lvl_info.max_seeds)
+            #seeds
+            if user.player.seeds < lvl_info.max_seeds:
+                seeds_to_apply = user.player.seeds + seeds_in_delta_time
+                user.player.seeds = min(seeds_to_apply,lvl_info.max_seeds)
 
-        user.player.last_updated_at = datetime.now(timezone.utc)
-        user.player.save()
+            user.player.last_updated_at = datetime.now(timezone.utc)
+            user.player.save()
