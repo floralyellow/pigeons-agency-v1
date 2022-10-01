711b25a6cbe23cfadb739521fa6cb491a80c2f99
Artr Ctrs <arthur.catrisse@hotmail.com>
2022-10-01 15:35:37 +0200

    feat: create abstract model for pigeons

--- backend/pigeon_app/models/pigeon.py
+++ backend/pigeon_app/models/pigeon.py
@@ -2,27 +2,11 @@ from django.db import models
 from rest_framework import serializers
 
 from .player import Player
+from .abstract_pigeon import AbstractPigeon
 
 
-class Pigeon(models.Model):
+class Pigeon(AbstractPigeon):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    pigeon_type = models.IntegerField(default=1)
-    name = models.CharField(max_length=30, default="test")
-    src = models.CharField(max_length=30, default="test")
-    pigeon_id = models.IntegerField(default=1)
-    lvl = models.IntegerField(default=1)
-    luck = models.IntegerField(default=1)
-    phys_atk = models.IntegerField(default=1)
-    magic_atk = models.IntegerField(default=1)
-    shield = models.IntegerField(default=0)
-    droppings_minute = models.IntegerField(default=1)
-    feathers = models.IntegerField(default=1)
-    creation_time = models.DateTimeField(null=True)
-    active_time = models.DateTimeField(null=True)
-    is_open = models.BooleanField(default=False)
-    is_sold = models.BooleanField(default=False)
-    is_in_team_A = models.BooleanField(default=False)
-    is_in_team_B = models.BooleanField(default=False)
 
 
 class PigeonSerializer(serializers.ModelSerializer):
