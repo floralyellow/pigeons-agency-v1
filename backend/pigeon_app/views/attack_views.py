178c3cd6b8138be98660dbd6f144d4dee60189db
ArtrCtrs <arthur.catrisse@hotmail.com>
2023-04-18 22:30:10 +0200

    fix response of attack

--- backend/pigeon_app/views/attack_views.py
+++ backend/pigeon_app/views/attack_views.py
@@ -1,7 +1,6 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
-from ..models.attack_pigeon import AttackPigeonSerializer
 from ..models.player import UserSerializer
 from ..services import attack_service, update_service
 from ..utils.validators import InputValidator
@@ -17,12 +16,16 @@ class AttackView(APIView):
         InputValidator.validate_is_int(target_id)
         InputValidator.validate_is_in_values(attack_team, ["A", "B"])
 
-        attack, pigeons = attack_service.attack_player(request.user, int(target_id), attack_team)
-        pigeons_to_send = AttackPigeonSerializer(pigeons, many=True).data
+        attack, attack_pigeons, defend_pigeons, defender = attack_service.attack_player(
+            request.user, int(target_id), attack_team
+        )
+
         message = {
             "user": UserSerializer(request.user).data,
             "attack": attack,
-            "attack_pigeons": pigeons_to_send,
+            "attack_pigeons": list(attack_pigeons.values()),
+            "defend_pigeons": list(defend_pigeons.values()),
+            "defender": UserSerializer(defender).data,
         }
 
         return JsonResponse({"message": message})
