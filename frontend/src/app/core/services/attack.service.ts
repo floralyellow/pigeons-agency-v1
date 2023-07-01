5a7eb63d82db7093053e54109a578cae69894fd0
ArtrCtrs <arthur.catrisse@hotmail.com>
2023-07-01 15:18:34 +0200

    wip attack front

--- frontend/src/app/core/services/attack.service.ts
+++ frontend/src/app/core/services/attack.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
+import { Pigeon } from '../models';
+import { Attack } from '../models/attack';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +16,29 @@ export class AttackService {
     return new Promise((resolve, reject) => {
       this.http.get(environment.apiBaseUrl + '/attacklist')
         .subscribe((res: { 'message': { user: User, users: User[] } }) => {
+          resolve(res.message)
+        }, err => {
+          reject(err);
+        });
+    });
+  }
+
+  postAttack(team : 'A'|'B', attackedUserId : number): Promise<{
+    user: User, 
+    users: User[], 
+    attack:Attack, 
+    attack_pigeons:Pigeon[], 
+    defend_pigeons:Pigeon[]
+  }> {
+    return new Promise((resolve, reject) => {
+      this.http.post(environment.apiBaseUrl + '/attack', `a_team=${team}&u_id=${attackedUserId}`)
+        .subscribe((res: { 'message': { 
+          user: User, 
+          users: User[], 
+          attack: Attack, 
+          attack_pigeons:Pigeon[], 
+          defend_pigeons:Pigeon[] 
+        } }) => {
           resolve(res.message)
         }, err => {
           reject(err);
--- frontend/src/app/pages/components/attack/attack.component.ts
+++ frontend/src/app/pages/components/attack/attack.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'src/app/core/models';
+import { Attack } from 'src/app/core/models/attack';
 import { AttackService } from 'src/app/core/services/attack.service';
+import { ModalComponent } from 'src/app/ui';
+import {
+  faHatWizard,
+  faShieldAlt,
+  faFistRaised
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-attack',
@@ -8,13 +15,36 @@ import { AttackService } from 'src/app/core/services/attack.service';
   styleUrls: ['./attack.component.scss']
 })
 export class AttackComponent implements OnInit {
+  faHatWizard = faHatWizard;
+  faFistRaised = faFistRaised;
+  faShieldAlt = faShieldAlt;
+  modalTitle: string;
+  modalContent: string;
   users : User[]
   currentUser : User
+  attackResult : Attack
+  @ViewChild(ModalComponent) modal;
   constructor( private attackService : AttackService){
 
   }
-  async ngOnInit() {
-    this.users = (await this.attackService.getUsers()).users
-    this.currentUser = (await this.attackService.getUsers()).user
+  ngOnInit() {
+    this.attackService.getUsers().then(res => {
+      this.users = res.users
+      this.currentUser = res.user
+    })
+  }
+
+  attackButtonAction(team: 'A'|'B', targetId:number) {
+    this.attackService.postAttack(team,targetId).then(res=>{
+      this.users = res.users
+      this.attackResult = res.attack
+      console.log(this.attackResult)
+      this.modal.toggleModal();
+      this.modalTitle = (res.attack.winner_id === this.currentUser.id)? 
+        `Victory ! You won ${res.attack.stolen_droppings} droppings !` : 
+        'Defeat ! You lost ${this.currentAdventure.reward_droppings} droppings !'
+      this.modalHeaderBackground = (result.adventure_attack.is_victory === true)? 
+        'has-background-success' : 'has-background-danger';
+    })
   }
 }
