f24ea97c9d5fece9da9b0d3acf46dec95dbf6dad
ArtrCtrs <arthur.catrisse@hotmail.com>
2023-08-09 20:18:20 +0200

    several fixes

--- frontend/src/app/ui/expedition-card/expedition-card.component.ts
+++ frontend/src/app/ui/expedition-card/expedition-card.component.ts
@@ -1,18 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-expedition-card',
   templateUrl: './expedition-card.component.html',
   styleUrls: ['./expedition-card.component.scss']
 })
-export class ExpeditionCardComponent implements OnInit {
+export class ExpeditionCardComponent  {
   @Input() info: {
     name: string,
     duration: number,
     seeds: number
   };
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
